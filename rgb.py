8c0bb6a220a8ee8f25a55861b83a526c3efafc53
Parker Novakovic <m184668@usna.edu>
2018-01-27 14:11:27 -0500

    Added Lab 1 arduino and pggame code

---
+++ rgb.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import serial
+import pygame
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((500,400),0,32)
+pygame.display.set_caption('Lab1 Color Wheel')
+
+s= serial.Serial("/dev/ttyACM0")
+WHITE = (255,255,255)
+DISPLAYSURF.fill(WHITE)
+while(True):
+  l = s.readline()
+  colors = l.rstrip().split(",")
+  rgb = [ int(val) for val in colors]
+  pygame.draw.rect(DISPLAYSURF, rgb, ((500,400), 0, 32))
+  print rgb
+
+
+
+  
+  
+
