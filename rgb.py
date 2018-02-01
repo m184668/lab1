8a6bae3fa5bb916ebdfdcb04001fef81ab693b19
Parker Novakovic <m184668@usna.edu>
2018-01-31 21:08:49 -0500

    Final Lab 1 in rgb.py

--- rgb.py
+++ rgb.py
@@ -1,25 +1,37 @@
 #! /usr/bin/python
 
 import serial
-import pygame
+import pygame, sys
+from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((500,400),0,32)
+DISPLAYSURF = pygame.display.set_mode((400,400),0,32)
 pygame.display.set_caption('Lab1 Color Wheel')
 
-s= serial.Serial("/dev/ttyACM0")
+s = serial.Serial("/dev/ttyACM0")
 WHITE = (255,255,255)
-DISPLAYSURF.fill(WHITE)
-while(True):
+DISPLAYSURF.fill(WHITE) 
+pygame.display.update()
+while True:
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
   l = s.readline()
   colors = l.rstrip().split(",")
+
   try:
-    rgb = [ int(val) for val in colors]
-    DISPLAYSURF.fill(rgb[0],rgb[1],rgb[2])
-    pygame.display.update()
+   Color_G = int(colors[0])
+   Color_B = int(colors[1])
+   Color_R = int(colors[2])
+   DISPLAYSURF.fill((Color_R,Color_G, Color_B))
+   pygame.display.update()
+
   except ValueError:
     print("Pass")
-  print rgb
+  
+   
+    
  
 
 
